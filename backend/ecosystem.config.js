082b4f687dba965a06186cae7f5053f259bdc18b
Natalia Linkova <linkova1109@yandex.ru>
2022-10-03 14:48:55 +1000

    heroku

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,31 +1,15 @@
 module.exports = {
   apps: [
     {
-      script: "app.js",
-      watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
+      name: "app",
+      script: "./app.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
-  env: {
-    NODE_ENV: "development",
-  },
-  env_production: {
-    NODE_ENV: "production",
-  },
-  deploy: {
-    production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-    },
-  },
 };
